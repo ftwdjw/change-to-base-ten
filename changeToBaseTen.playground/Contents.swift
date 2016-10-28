ff6ab725cd7991c80c2db7e3aa2c04803c4348fe
ftwdjw <ftwdjw@gmail.com>
2016-10-28 12:56:31 -0500

    added digit check of number to be converted is less than base.

--- changeToBaseTen.playground/Contents.swift
+++ changeToBaseTen.playground/Contents.swift
@@ -1,82 +1,85 @@
-//: Change from base 16 to base 10
+//: Change from base 2-16 to base 10
 
 import UIKit
-
-var str = "FFEE1010"//base 16
-var strArray = [Int]()
+//number to convert is expressed as a string since any base higher than 10 requires an alphabetic character
+var str = "FFFF"//number to convert expressed as a string
+var base = 16 //any base from 2 to 16
+/*******************/
+var intArray = [Int]()
 
 for character in str.characters {
     
 //print(character)
     
+   //this switch statement converts the string representing the number to be converted to an integer array
     switch character {
     case "0":
-        print("0", terminator:"")
-        strArray.append(0)
+        //print("0", terminator:"")
+        intArray.append(0)
         
     case "1":
-        print("1", terminator:"")
-         strArray.append(1)
+        //print("1", terminator:"")
+         intArray.append(1)
         
     case "2":
-        print("2", terminator:"")
-         strArray.append(2)
+        //print("2", terminator:"")
+        intArray.append(2)
         
     case "3":
-        print("3", terminator:"")
-         strArray.append(3)
+        //print("3", terminator:"")
+         intArray.append(3)
         
     case "4":
-        print("4", terminator:"")
-         strArray.append(4)
+        //print("4", terminator:"")
+         intArray.append(4)
         
     case "5":
-        print("5", terminator:"")
-         strArray.append(5)
+        //print("5", terminator:"")
+         intArray.append(5)
         
     case "6":
-        print("6", terminator:"")
-         strArray.append(6)
+        //print("6", terminator:"")
+         intArray.append(6)
         
     case "7":
-        print("7", terminator:"")
-         strArray.append(7)
+        //print("7", terminator:"")
+         intArray.append(7)
         
     case "8":
-        print("8", terminator:"")
-         strArray.append(8)
+        //print("8", terminator:"")
+         intArray.append(8)
         
     case "9":
-        print("9", terminator:"")
-         strArray.append(9)
+        //print("9", terminator:"")
+         intArray.append(9)
         
     case "A":
-        print("A", terminator:"")
-         strArray.append(10)
+        //print("A", terminator:"")
+         intArray.append(10)
         
     case "B":
-        print("B", terminator:"")
-         strArray.append(11)
+        //print("B", terminator:"")
+         intArray.append(11)
         
     case "C":
-        print("C", terminator:"")
-         strArray.append(12)
+        //print("C", terminator:"")
+         intArray.append(12)
         
     case "D":
-        print("D", terminator:"")
-         strArray.append(13)
+        //print("D", terminator:"")
+         intArray.append(13)
         
     case "E":
         print("E", terminator:"")
-        strArray.append(14)
+        intArray.append(14)
     case "F":
         
-        print("F", terminator:"")
-        strArray.append(15)
+        //print("F", terminator:"")
+        intArray.append(15)
         
         
     default:
-        print("this is not a digit", terminator:"")
+        print("this is not a valid digit", terminator:"")
     }
 
     
@@ -84,16 +87,23 @@ for character in str.characters {
 
 //print(strArray)
 
-let digits=strArray.count
+let digits=intArray.count
 var sum=0.0
 var x = 0.0
 var y = 0.0
+var number:Int = 0
 
 for index in (0..<digits){
     
-    x=Double(strArray[digits-index-1])
+    number=intArray[digits-index-1]
+    
+    //print(number)
+    
+    assert( number < base , "all digits of number to convert must be less than the base of the number" )
+    
+    x=Double(intArray[digits-index-1])
     //print(x)
-    y=pow(16.0,Double(digits-index-1))
+    y=pow(Double(base),Double(digits-index-1))
     //print(y)
     
     sum=sum+x*y
@@ -102,6 +112,6 @@ for index in (0..<digits){
 
 //print(sum)
 
-print("\n\(str) base 16 = ", terminator:"")
+print("\n\(str) base \(base) = ", terminator:"")
 print(" \(sum) base \(10)")
 
